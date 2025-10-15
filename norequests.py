5d0cebc1a2373c4f7e13ab674f2a4d4ae2cb0923
gatopeich <7722268+gatopeich@users.noreply.github.com>
2025-10-15 03:56:26 +0100

    Add full implementation of norequests.py

--- norequests.py
+++ norequests.py
@@ -1,14 +1,51 @@
-# norequests.py
+import urllib.request
+import urllib.parse
+import json
+import warnings
 
-class NoRequests:
-    def get(self, url):
-        return "No response from {url}".format(url=url)
+class Response:
+    def __init__(self, resp):
+        self.status_code = resp.getcode()
+        self.headers = dict(resp.getheaders())
+        self._raw = resp.read()
+        self.url = resp.geturl()
 
-    def post(self, url, data):
-        return "No response from {url} with data {data}".format(url=url, data=data)
+    @property
+    def content(self):
+        return self._raw
 
-# Example usage
-if __name__ == '__main__':
-    client = NoRequests()
-    print(client.get('http://example.com'))
-    print(client.post('http://example.com', {'key': 'value'}))
+    @property
+    def text(self):
+        return self._raw.decode('utf-8', errors='replace')
+
+    def json(self):
+        return json.loads(self.text)
+
+def _request(method, url, *, params=None, data=None, json_=None, headers=None, timeout=9):
+    if not url.startswith("https://"):
+        warnings.warn("You are making a non-SSL request. This is insecure!", UserWarning)
+    if params:
+        url += '?' + urllib.parse.urlencode(params)
+    if json_ is not None:
+        data = json.dumps(json_).encode('utf-8')
+        headers = headers or {}
+        headers['Content-Type'] = 'application/json'
+    elif isinstance(data, dict):
+        data = urllib.parse.urlencode(data).encode('utf-8')
+        headers = headers or {}
+        headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    req = urllib.request.Request(url, method=method.upper(), data=data, headers=headers or {})
+    with urllib.request.urlopen(req, timeout=timeout) as resp:
+        return Response(resp)
+
+def get(url, *, params=None, headers=None, timeout=9):
+    return _request('GET', url, params=params, headers=headers, timeout=timeout)
+
+def post(url, *, data=None, json=None, headers=None, timeout=9):
+    return _request('POST', url, data=data, json_=json, headers=headers, timeout=timeout)
+
+def put(url, *, data=None, json=None, headers=None, timeout=9):
+    return _request('PUT', url, data=data, json_=json, headers=headers, timeout=timeout)
+
+def delete(url, *, headers=None, timeout=9):
+    return _request('DELETE', url, headers=headers, timeout=timeout)
